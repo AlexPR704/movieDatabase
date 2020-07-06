30cfdd63c145e9bae7970d6d10d3f65e1668e985
Alexander Garcia <alexg97343@gmail.com>
2020-07-06 19:51:23 -0400

    acquired the api key for the movies and placed in the search.js file

--- src/components/search.js
+++ src/components/search.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 function Search () {
+    const apiurl = "http://www.omdbapi.com/?apikey=trilogy";
+
     return (
       <section className="seachbox-wrap">
           <input type="text" placeholder="Search for a movie..." className="searchbox" />
