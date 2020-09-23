557c17afc95426d88b04716941b3f3a560002449
Alexander Garcia <alexg97343@gmail.com>
2020-09-22 23:03:26 -0400

    Updates

---
+++ precache-manifest.d18d8e50b937460507ce3171661bc152.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32d4d4d8380b0e5479ae16990ef06f17",
+    "url": "/movieDatabase/index.html"
+  },
+  {
+    "revision": "8f84efa250397bcbdf0b",
+    "url": "/movieDatabase/static/css/main.4c76ffcc.chunk.css"
+  },
+  {
+    "revision": "320e3b23404616577bcc",
+    "url": "/movieDatabase/static/js/2.45c8c0e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movieDatabase/static/js/2.45c8c0e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f84efa250397bcbdf0b",
+    "url": "/movieDatabase/static/js/main.9282f8e0.chunk.js"
+  },
+  {
+    "revision": "c720f316b09be3c48142",
+    "url": "/movieDatabase/static/js/runtime-main.583cf18f.js"
+  }
+]);
